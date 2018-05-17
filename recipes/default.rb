1869813898cd3157d37abb472001096d31269734
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-05-17 17:59:19 +0900

    use apt_repository to simplify

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,35 +10,18 @@ package 'software-properties-common'
 # sudo add-apt-repository ppa:openjdk-r/ppa
 execute 'add-apt-repository ppa:openjdk-r/ppa'
 
-# wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-remote_file '/tmp/jenkins-ci.org.key' do
-  source 'https://pkg.jenkins.io/debian/jenkins-ci.org.key'
-  notifies :run, 'execute[apt-key add /tmp/jenkins-ci.org.key]', :immediately
-end
-
-execute 'apt-key add /tmp/jenkins-ci.org.key' do
-  action :nothing
-end
-
-# sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-file '/etc/apt/sources.list.d/jenkins.list' do
-  content 'deb http://pkg.jenkins.io/debian-stable binary/'
-  notifies :run, 'execute[apt-get update]', :immediately
-end
-
-# sudo apt-get update
-execute 'apt-get update' do
-  action :nothing
-end
+execute 'apt-get update'
 
 # sudo apt-get install openjdk-8-jre-headless
 package 'openjdk-8-jre-headless'
 
-# sudo apt-get install jenkins
-package 'jenkins' do
-  action :remove
+apt_repository 'jenkins' do
+  uri          'http://pkg.jenkins-ci.org/debian'
+  distribution 'binary/'
+  key          'https://jenkins-ci.org/debian/jenkins-ci.org.key'
 end
 
+# sudo apt-get install jenkins
 package 'jenkins' do
   options '--force-yes'
 end
